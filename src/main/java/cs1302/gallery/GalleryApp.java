5d73202c3393abc1c6bddadac63c40dda92a6d66
Kofi Reeves-Miller <kmr99059@uga.edu>
2021-04-11 23:04:03 -0400

    getting this thread prpped

--- src/main/java/cs1302/gallery/GalleryApp.java
+++ src/main/java/cs1302/gallery/GalleryApp.java
@@ -27,85 +27,19 @@ public class GalleryApp extends Application {
         menu.setMaxWidth(900);
         menu.getMenus().add(music);
 
-        VBox icons = new VBox();
-        HBox i = new HBox();
-        i.setAlignment(Pos.CENTER);
-        i.setMinWidth(50);
-
-
-        HBox j = new HBox();
-        j.setAlignment(Pos.CENTER);
-
-
-        HBox k = new HBox();
-        k.setAlignment(Pos.CENTER);
-
-        HBox l = new HBox();
-        l.setAlignment(Pos.CENTER);
-
-
-        HBox h = new HBox();
-        h.setAlignment(Pos.CENTER);
 
         Thread task = new Thread(() -> {
             System.out.println("start");
-            hbox(h);
-            System.out.println("h");
-            hbox(i);
-            hbox(j);
-            hbox(k);
-            hbox(l);
+
             System.out.println("end");
         });
         task.start();
 
         System.out.println("yo");
 
-/*        Thread task2 = new Thread(() -> {
-              for (int loop = 0; loop < 5; loop++) {
-                  Thumbnail icon = new Thumbnail();
-                  i.getChildren().add(icon);
-                  array[loop + 5] = icon;
-                  // icons.getChildren().add(i);
-              }
-        });
-        task2.start();
-
-        Thread task3 = new Thread(() -> {
-            for (int loop = 0; loop < 5; loop++) {
-                Thumbnail icon = new Thumbnail();
-                j.getChildren().add(icon);
-                array[loop + 10] = icon;
-                //icons.getChildren().add(j);
-            }
-        });
-        task3.start();
-
-        Thread task4 = new Thread(() -> {
-            for (int loop = 0; loop < 5; loop++) {
-                Thumbnail icon = new Thumbnail();
-                k.getChildren().add(icon);
-                array[loop + 15] = icon;
-                //icons.getChildren().add(k);
-            }
-        });
-        task4.start();
-
-        Thread task5 = new Thread(() -> {
-        for (int loop = 0; loop < 5; loop++) {
-            Thumbnail icon = new Thumbnail();
-            l.getChildren().add(icon);
-            array[loop + 20] = icon;
-            //icons.getChildren().add(l);
-             }
-        });
-        task5.start();*/
-
-            icons.getChildren().addAll(h,i,j,k,l);
-            System.out.println("jk");
 
         HBox mun = new HBox(menu);
-        VBox pane = new VBox(2, mun, new SearchBar(this), icons, new Progress());
+        VBox pane = new VBox(2, mun, new SearchBar(this), tile, new Progress());
         pane.setMinWidth(300);
         pane.setMaxWidth(900);
 
